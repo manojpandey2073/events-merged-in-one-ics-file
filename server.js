b3a4f5932fd806f5a2fb88093e9d480fe785f6df
manoj pandey <52687676+manojpandey2073@users.noreply.github.com>
2025-01-08 12:24:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware to parse JSON
 app.use(bodyParser.json());
