6dda225f02e8eddf940b52677e24e913e68a0665
manojpandey2073 <manojpandey2073@gmail.com>
2025-01-15 16:38:56 +0530

    hi

--- public/js/index.js
+++ public/js/index.js
@@ -451,7 +451,7 @@ function getFormData() {
                     lastArray.push({
                         title: item.data[`event_name_${index + 1}`],
                         description: item.data[`event_description_${index + 1}`],
-                        start: [dateStart.getFullYear(), dateStart.getMonth() + 1, dateStart.getDate(), dateStart.getHours(), dateStart.getMinutes()],
+                        start: new Date(Date.UTC(dateStart.getUTCFullYear(), dateStart.getUTCMonth(), dateStart.getUTCDate(), dateStart.getUTCHours(), dateStart.getUTCMinutes())).toISOString(),
                         location: item.data[`event_location_${index + 1}`],
                         duration: { hours: Number(item.data[`event_hour_${index + 1}`]), minutes: Number(item.data[`event_min_${index + 1}`]) },
                         organizer: {name: item.data[`event_organiser_name_${index + 1}`], email: item.data[`event_organiser_email_${index + 1}`]}
@@ -462,7 +462,7 @@ function getFormData() {
                     lastArray.push({
                         title: item.data[`event_name_${index + 1}`],
                         description: item.data[`event_description_${index + 1}`],
-                        start: [dateStart.getUTCFullYear(), dateStart.getUTCMonth() + 1, dateStart.getUTCDate(), dateStart.getUTCHours(), dateStart.getUTCMinutes()],
+                        start: new Date(Date.UTC(dateStart.getUTCFullYear(), dateStart.getUTCMonth(), dateStart.getUTCDate(), dateStart.getUTCHours(), dateStart.getUTCMinutes())).toISOString(),
                         location: item.data[`event_location_${index + 1}`],
                         duration: { hours: Number(item.data[`event_hour_${index + 1}`]), minutes: Number(item.data[`event_min_${index + 1}`]) },
                         organizer: { name: item.data[`event_organiser_name_${index + 1}`], email: item.data[`event_organiser_email_${index + 1}`] }
